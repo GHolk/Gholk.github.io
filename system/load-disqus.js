058e996b9fa37147b1edcd19419002d9a5f287b2
gholk <c34031328@mail.ncku.edu.tw>
2017-10-04 22:56:22 +0800

    use button load disqus

--- system/load-disqus.js
+++ system/load-disqus.js
@@ -1,12 +1,22 @@
-var disqus_config = function () {
+let disqus_config = function () {
     this.page.url = window.location
     this.page.identifier = window.location.toString().replace(/.*\//,'')
 }
 
 void function() {
-    var d = document
-    var s = d.createElement('script')
-    s.src = 'https://gholk-gh.disqus.com/embed.js'
-    s.setAttribute('data-timestamp', Number(new Date()))
-    d.body.appendChild(s)
+    const loadButton = document.querySelector('#disqus_thread button')
+    loadButton.addEventListener(
+        'click',
+        function loadButtonClick() {
+            this.remove()
+            void function () {
+                const d = document
+                const s = d.createElement('script')
+                s.src = 'https://gholk-gh.disqus.com/embed.js'
+                s.setAttribute('data-timestamp', Number(new Date()))
+                d.body.appendChild(s)
+            }()
+        },
+        {once: true}
+    )
 }()

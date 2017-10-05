871c7f31e866f4fb76f7e6a2017b1366adb1c468
gholk <c34031328@mail.ncku.edu.tw>
2017-10-05 09:43:14 +0800

    [html] add js eval function

---
+++ ext/eval-prefix-js.js
@@ -0,0 +1,30 @@
+
+void function () {
+    let codeNode = document.querySelectorAll(':not(pre) code')
+    codeNode = Array.from(codeNode)
+        .filter((code) => /^js /.test(code.textContent))
+    codeNode.forEach((code) => {
+        const jsCode = code.textContent.replace(/^js /, '')
+        let evalResult
+        try {
+            evalResult = eval(jsCode)
+        }
+        catch (evalError) {
+            evalResult = null
+        }
+        finally {
+            if (evalResult) {
+                code.title = jsCode
+                showResult(code, evalResult)
+            }
+        }
+    })
+
+    function showResult(node, result) {
+        if (result instanceof Node) node.appendChild(result)
+        else if (result instanceof Array) {
+            result.forEach((x) => showResult(node, x))
+        }
+        else node.textContent = result
+    }
+}()

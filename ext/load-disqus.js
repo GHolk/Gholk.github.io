0b13a0e9f138148c3076ff4a7512a90ee94017ad
gholk <c34031328@mail.ncku.edu.tw>
2017-10-07 00:24:54 +0800

    [web] remove disqus button, load directly

--- ext/load-disqus.js
+++ ext/load-disqus.js
@@ -4,19 +4,9 @@ let disqus_config = function () {
 }
 
 void function() {
-    const loadButton = document.querySelector('#disqus_thread button')
-    loadButton.addEventListener(
-        'click',
-        function loadButtonClick() {
-            this.remove()
-            void function () {
-                const d = document
-                const s = d.createElement('script')
-                s.src = 'https://gholk-gh.disqus.com/embed.js'
-                s.setAttribute('data-timestamp', Number(new Date()))
-                d.body.appendChild(s)
-            }()
-        },
-        {once: true}
-    )
+    const d = document
+    const s = d.createElement('script')
+    s.src = 'https://gholk-gh.disqus.com/embed.js'
+    s.setAttribute('data-timestamp', Number(new Date()))
+    d.body.appendChild(s)
 }()

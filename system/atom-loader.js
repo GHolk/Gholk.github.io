a6c943c9f6dde6e83ea2f8863ac5f9a0f5d48532
gholk <85619andy@gmail.com>
2017-12-17 23:22:58 +0800

    [system] add content section

--- system/atom-loader.js
+++ system/atom-loader.js
@@ -40,15 +40,23 @@ ${tags}
         const searchOgImage = loader.selector('meta[property="og:image"]')
         if (searchOgImage.length > 0) {
             const meta = searchOgImage.last()
-            const link = this.selector('<link rel="alternate">')
+            const link = this.selector('<link rel="enclosure">')
             link.attr('href', meta.attr('content'))
-            link.attr('type', meta.attr('type'))
             entry.append(link)
         }
-            
+
+        const content = this.selector('<content type="html">')
+        content.text(escapeHtml(loader.main))
+        entry.append(content)
+
         this.selector('entry:first-of-type').before(entry)
         function tagToAtom(tag) {
             return `<category term="${tag}" />`
+        }
+        function escapeHtml(html) {
+            return html.replace(/&/g, '&amp;')
+                .replace(/>/g, '&gt;')
+                .replace(/</g, '&lt;')
         }
     }
 }

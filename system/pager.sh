9801804827e94474ef945c9e3fb75d22dcdf588e
gholk <85619andy@gmail.com>
2018-11-29 13:51:56 +0800

    unify change log

--- system/pager.sh
+++ system/pager.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
-html_path=$1
+text_path=$1
+html_path=$2
 
 if [ -e $html_path ]
-then exist_old_html=1
+then 
+    node updateHTML.js $html_path
+    change_log=$(true | vipe)
+    if [ $(echo "$change_log" | wc -l) -gt 1 ]
+    then
+        node update-to-atom.js $html_path "$change_log"
+    else
+        git diff $text_path | node update-to-atom.js $html_path "$change_log"
+    fi
+else
+    cp template.html $html_path
+    node add-to-atom.js $html_path
+    node updateRel.js $html_path
 fi
 
-if ! [ $exist_old_html ]
-then cp template.html $html_path
-fi
-
-node updateHTML.js $html_path
-node add-to-atom.js $html_path
-
-if ! [ $exist_old_html ]
-then node updateRel.js $html_path
-fi
-
+commit_message="[text] $(basename -s .txt $text_path | sed 's/-/ /g') $change_log"
+git commit -a -m "$commit_message" $text_path $html_path

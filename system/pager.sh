c9a97e94fa1490d29ad148cf76a83c1bec3fbb4f
gholk <85619andy@gmail.com>
2018-11-30 12:47:37 +0800

    [system] devide add-to-atom and generate html

--- system/pager.sh
+++ system/pager.sh
@@ -1,23 +1,10 @@
 #!/bin/sh
 
-text_path=$1
-html_path=$2
+html_path=$1
 
 if [ -e $html_path ]
 then 
-    node updateHTML.js $html_path
-    change_log=$(true | vipe)
-    if [ $(echo "$change_log" | wc -l) -gt 1 ]
-    then
-        node update-to-atom.js $html_path "$change_log"
-    else
-        git diff $text_path | node update-to-atom.js $html_path "$change_log"
-    fi
-else
     cp template.html $html_path
-    node add-to-atom.js $html_path
-    node updateRel.js $html_path
 fi
+node updateHTML.js $html_path
 
-commit_message="[text] $(basename -s .txt $text_path | sed 's/-/ /g') $change_log"
-git commit -a -m "$commit_message" $text_path $html_path

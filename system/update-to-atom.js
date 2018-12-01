57c985adabc02d2a1532f9973c08f4ae07a2d3fc
gholk <85619andy@gmail.com>
2018-12-01 22:02:25 +0800

    [system] no apply diff class on commit description

--- system/update-to-atom.js
+++ system/update-to-atom.js
@@ -7,9 +7,16 @@ const changeLog = process.argv[3].trim().split(/\n/g)
 const atom = new AtomLoader()
 const post = new HTMLLoader(htmlPath)
 
-function addToAtom(title, description) {
-    const $summary = post.selector('<pre>').addClass('diff').text(description)
-    post.description = post.selector.html($summary)
+function addToAtomDiff(title, diff) {
+    const $diff = post.selector('<pre>').addClass('diff').text(diff)
+    post.description = post.selector.html($diff)
+    post.file += '#' + title
+    atom.add(post)
+    atom.write()
+}
+
+function addToAtomDescription(title, description) {
+    post.description = post.selector('<pre>').text(description).html()
     post.file += '#' + title
     atom.add(post)
     atom.write()
@@ -25,13 +32,13 @@ function extractDiffFromGit(gitLog) {
 const changeTitle = changeLog[0].replace(/\s/g, '-')
 if (changeLog.length > 1) {
     const changeDescription = changeLog.slice(1).join('\n')
-    addToAtom(changeTitle, changeDescription)
+    addToAtomDescription(changeTitle, changeDescription)
 }
 else {
     let gitLog = ''
     process.stdin.on('data', data => gitLog += data)
     process.stdin.on('end', () => {
-        const changeDescription = extractDiffFromGit(gitLog)
-        addToAtom(changeTitle, changeDescription)
+        const diff = extractDiffFromGit(gitLog)
+        addToAtomDiff(changeTitle, diff)
     })
 }
